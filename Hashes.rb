3e327c1108b8c2c2c19d5e367977b7e8932c808a
MbuviM <mwendembuvi10@gmail.com>
2024-02-22 06:03:29 +0300

    Hashes Practice

---
+++ Hashes.rb
@@ -0,0 +1,13 @@
+#{Hashes are key-value pairs}
+
+cities = {
+  "Nairobi" => 'NBO',
+  "Kisumu" => 'KSM',
+  "Nakuru" => 'NAK',
+  "Eldoret" => 'ELD',
+  "Mombasa" => "MSA"
+}
+
+#{Prints out the value of the key cities}
+puts(cities["Eldoret"])
+
