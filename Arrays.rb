3e966a05d4b0f44b59f6677e2f22f13d9a9dbe0b
MbuviM <mwendembuvi10@gmail.com>
2024-02-22 06:02:41 +0300

    Arrays practice

---
+++ Arrays.rb
@@ -0,0 +1,14 @@
+friends = Array["Oscar", "Apolo", "Mel", "Beaty" , "Nile","Mwangi",]
+
+puts(friends)
+puts(friends.class)
+puts(friends.length) #{Checking the lenght of the array}
+puts(friends.include? "Rev") #{Checks if an element is part of an array}
+puts(friends.sort) #{Sorts an array}
+puts(friends.reverse) #{Reverses an array}
+
+#{Creating an array without knowing the elements to add}
+new_friends = Array.new
+
+new_friends[1] = "Komu"
+puts(new_friends)
