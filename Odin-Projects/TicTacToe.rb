b86fb703474ac9d695493c9141f61480b3b8c372
MbuviM <mwendembuvi10@gmail.com>
2024-03-05 04:27:00 +0300

    Tic Tac Toe Game

---
+++ Odin-Projects/TicTacToe.rb
@@ -0,0 +1,19 @@
+class TicTacToe
+    #{Initialization Method}
+    def initialize(player_1, player_2, board)
+        @player_1 = player_1
+        @player_2 = player_2
+        @board = Array.new(3) { Array.new(3, ' ') }
+    end
+
+    #{Player 1 Method}
+    def move_X()
+        @X = X
+        @O = O
+    end
+
+    #{Player 2 Method}
+    def move_Y()
+
+    end
+end
